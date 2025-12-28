22380fe43c33b0f11184b4d6e45bb8dc397d0485
Grzegorz Golonka <grzegorz.1.golonka@nokia.com>
2025-12-28 19:15:56 +0100

    [SS] remove obj dir and format reposities

--- Repositories/ConcertRepository.cs
+++ Repositories/ConcertRepository.cs
@@ -9,40 +9,44 @@ namespace SoundSphere.Repositories;
 public class ConcertRepository : IConcertRepository{
 
     public AppDatabaseContext _context;
-    public ConcertRepository(AppDatabaseContext context)
-    {
+
+    public ConcertRepository(AppDatabaseContext context) {
         _context = context;
     }
-    public async Task<IEnumerable<MusicConcert>> GetAllConcerts()
-    {
+
+    public async Task<IEnumerable<MusicConcert>> GetAllConcerts() {
         return await _context.Concerts.ToListAsync();
     }
-    public async Task<MusicConcert> GetConcertById(int id)
-    {
-        return await _context.Concerts.Include(concert => concert.Rating).FirstOrDefaultAsync(concert => concert.Id == id);
-    }
-    public async Task<MusicConcert> GetConcertByIdAsyncNoTracking(int id)
-    {
-        return await _context.Concerts.Include(concert => concert.Rating).AsNoTracking().FirstOrDefaultAsync(concert => concert.Id == id);
 
+    public async Task<MusicConcert> GetConcertById(int id) {
+        return await _context.Concerts
+            .Include(concert => concert.Rating)
+            .FirstOrDefaultAsync(concert => concert.Id == id);
     }
-    public bool AddNewConcert(MusicConcert musicConcert)
-    {
+
+    public async Task<MusicConcert> GetConcertByIdAsyncNoTracking(int id) {
+        return await _context.Concerts
+            .Include(concert => concert.Rating)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(concert => concert.Id == id);
+    }
+    
+    public bool AddNewConcert(MusicConcert musicConcert) {
         _context.Add(musicConcert);
         return Save();
     }
-    public bool UpdateExistingConcert(MusicConcert musicConcert)
-    {
+    
+    public bool UpdateExistingConcert(MusicConcert musicConcert) {
         _context.Update(musicConcert);
         return Save();
     }
-    public bool DeleteConcert(MusicConcert musicConcert)
-    {
+
+    public bool DeleteConcert(MusicConcert musicConcert){
         _context.Remove(musicConcert);
         return Save();
     }
-    public bool Save()
-    {
+
+    public bool Save() {
         var saved = _context.SaveChanges();
         return saved > 0 ? true : false;
     }
--- Repositories/TrackRepository.cs
+++ Repositories/TrackRepository.cs
@@ -6,46 +6,47 @@ using SoundSphere.Models;
 
 namespace SoundSphere.Repositories;
 
-public class TrackRepository : ITrackRepository
-{
+public class TrackRepository : ITrackRepository {
 
     public AppDatabaseContext _context;
-    public TrackRepository(AppDatabaseContext context)
-    {
+
+    public TrackRepository(AppDatabaseContext context) {
         _context = context;
     }
-    public async Task<IEnumerable<MusicTrack>> GetAllTracks()
-    {
+
+    public async Task<IEnumerable<MusicTrack>> GetAllTracks() {
         return await _context.Tracks.ToListAsync();
     }
-    public async Task<MusicTrack> GetTrackById(int id)
-    {
-        return await _context.Tracks.Include(track => track.Rating).FirstOrDefaultAsync(track => track.Id == id);
+    public async Task<MusicTrack> GetTrackById(int id) {
+        return await _context.Tracks
+            .Include(track => track.Rating)
+            .FirstOrDefaultAsync(track => track.Id == id);
     }
-    public async Task<MusicTrack> GetTrackByIdAsyncNoTracking(int id)
-    {
-        return await _context.Tracks.Include(track => track.Rating).AsNoTracking().FirstOrDefaultAsync(track => track.Id == id);
 
+    public async Task<MusicTrack> GetTrackByIdAsyncNoTracking(int id) {
+        return await _context.Tracks
+            .Include(track => track.Rating)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(track => track.Id == id);
     }
-    public bool AddNewTrack(MusicTrack musicTrack)
-    {
+
+    public bool AddNewTrack(MusicTrack musicTrack) {
         _context.Add(musicTrack);
         return Save();
     }
-    public bool UpdateExistingTrack(MusicTrack musicTrack)
-    {
+
+    public bool UpdateExistingTrack(MusicTrack musicTrack) {
         _context.Update(musicTrack);
         return Save();
     }
-    public bool DeleteTrack(MusicTrack musicTrack)
-    {
+
+    public bool DeleteTrack(MusicTrack musicTrack) {
         _context.Remove(musicTrack);
         return Save();
     }
-    public bool Save()
-    {
+
+    public bool Save() {
         var saved = _context.SaveChanges();
         return saved > 0 ? true : false;
     }
-
 }
